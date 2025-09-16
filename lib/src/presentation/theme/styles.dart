908e848682704e738faefaaf1b2b9b66dd90173a
Denis Titovets <den232titovets@yandex.ru>
2025-09-16 20:07:44 +0300

    Добавление стиля `defaultStyle` в Styles

--- lib/src/presentation/theme/styles.dart
+++ lib/src/presentation/theme/styles.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:prof_26_uikit/src/presentation/theme/palette.dart';
 
 class Styles {
   final Palette palette;
 
   Styles({required this.palette});
+
+  TextStyle get defaultStyle => TextStyle(color: palette.foregroundColor);
 }
