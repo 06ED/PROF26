8b5a0fd8d7bc8a872f584351dcffcf505d31ec1d
Denis Titovets <den232titovets@yandex.ru>
2025-09-26 10:07:25 +0300

    Создание MaterialAppWrapper

--- lib/prof_26_uikit.dart
+++ lib/prof_26_uikit.dart
@@ -3,3 +3,5 @@ library;
 export "src/presentation/theme/extension.dart";
 export "src/presentation/theme/palette.dart";
 export "src/presentation/theme/styles.dart";
+
+export "src/presentation/widgets/utils.dart";
---
+++ lib/src/presentation/widgets/utils.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:prof_26_uikit/prof_26_uikit.dart';
+
+class MaterialAppWrapper extends StatelessWidget {
+  final Widget? widget;
+
+  const MaterialAppWrapper({super.key, required this.widget});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(500, 1000),
+      builder: (_, __) {
+        return MaterialApp(
+          theme: ThemeData().copyWith(
+            extensions: [CustomTheme(palette: LightPalette())],
+          ),
+          darkTheme: ThemeData().copyWith(
+            extensions: [CustomTheme(palette: DarkPalette())],
+          ),
+          debugShowCheckedModeBanner: false,
+          home: Builder(
+            builder: (BuildContext context) {
+              var theme = CustomTheme.of(context);
+              return Scaffold(
+                backgroundColor: theme.palette.backgroundColor,
+                resizeToAvoidBottomInset: false,
+                body: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 36.w,
+                    vertical: 64.h,
+                  ),
+                  child: Center(
+                    child:
+                        widget ??
+                        CircularProgressIndicator(
+                          backgroundColor: Colors.transparent,
+                          color: theme.palette.foregroundColor,
+                        ),
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
