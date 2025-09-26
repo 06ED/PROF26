9bb7444a0a4fe09d44ce8d729922d53502ae3540
Denis Titovets <den232titovets@yandex.ru>
2025-09-26 11:08:38 +0300

    Добавление `BigButtonWidget` на тестовый экран

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:prof_26_uikit/prof_26_uikit.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,11 +8,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = CustomTheme.of(context);
+    final text = "Подтвердить";
     return Scaffold(
       backgroundColor: theme.palette.backgroundColor,
       resizeToAvoidBottomInset: false,
       body: Center(
-        child: Text("Hello world!", style: theme.styles.defaultStyle),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 36.w, vertical: 64.h),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 32.h,
+            children: [
+              BigButtonWidget.filled(theme: theme, onTap: () {}, text: text),
+              BigButtonWidget.filled(theme: theme, onTap: null, text: text),
+              BigButtonWidget.outlined(theme: theme, onTap: () {}, text: text),
+              BigButtonWidget.simple(theme: theme, onTap: () {}, text: text),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/presentation/widgets/application.dart
+++ lib/presentation/widgets/application.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:prof_26_app/presentation/pages/home.dart';
 import 'package:prof_26_uikit/prof_26_uikit.dart';
 
@@ -6,14 +7,17 @@ class Application extends StatelessWidget {
   const Application({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    theme: ThemeData().copyWith(
-      extensions: [CustomTheme(palette: LightPalette())],
+  Widget build(BuildContext context) => ScreenUtilInit(
+    designSize: Size(375, 812),
+    builder: (_, __) => MaterialApp(
+      theme: ThemeData().copyWith(
+        extensions: [CustomTheme(palette: LightPalette())],
+      ),
+      darkTheme: ThemeData().copyWith(
+        extensions: [CustomTheme(palette: DarkPalette())],
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     ),
-    darkTheme: ThemeData().copyWith(
-      extensions: [CustomTheme(palette: DarkPalette())],
-    ),
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
   );
 }
