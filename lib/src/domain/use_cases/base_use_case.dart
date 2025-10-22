6abb7b90a30500f407ec38a44b636de2a287e4da
Denis Titovets <den232titovets@yandex.ru>
2025-10-22 21:40:04 +0300

    Добавление запроса `getGoogle` в `BaseUseCase`

--- lib/src/domain/use_cases/base_use_case.dart
+++ lib/src/domain/use_cases/base_use_case.dart
@@ -3,4 +3,15 @@ import 'package:prof_26_query/prof_26_query.dart';
 
 class BaseUseCase {
   final Client _client = Client(Dio());
+
+  Future<void> getGoogle({
+    required void Function(void) onResponse,
+    required void Function(Exception) onError,
+  }) async {
+    await queryHelper(
+      request: () => _client.getGoogle(),
+      onResponse: onResponse,
+      onError: onError,
+    );
+  }
 }
