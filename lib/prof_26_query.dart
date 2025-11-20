95a2bd4e93623f139c7e1a031381d6ec8dc832e4
Denis Titovets <den232titovets@yandex.ru>
2025-11-20 21:29:31 +0300

    Добавление `TestQuery`

--- lib/prof_26_query.dart
+++ lib/prof_26_query.dart
@@ -1,10 +1,10 @@
 export "src/data/models/auth_model.dart";
 export "src/data/models/item_model.dart";
 
-export "src/data/storage/storage.dart";
-
 export "src/data/repository/client.dart";
 export "src/data/repository/repository.dart";
+
+export "src/data/storage/storage.dart";
 
 export "src/domain/services/query_helper.dart";
 
--- test/prof_26_query_test.dart
+++ test/prof_26_query_test.dart
@@ -1,12 +1,51 @@
-// import 'package:flutter_test/flutter_test.dart';
-//
-// import 'package:prof_26_query/prof_26_query.dart';
-//
-// void main() {
-//   test('adds one to input values', () {
-//     final calculator = Calculator();
-//     expect(calculator.addOne(2), 3);
-//     expect(calculator.addOne(-7), -6);
-//     expect(calculator.addOne(0), 1);
-//   });
-// }
+import "package:flutter_test/flutter_test.dart";
+import "package:prof_26_query/prof_26_query.dart";
+
+String testString = DateTime.now().microsecondsSinceEpoch.toString();
+
+Map<String, String> get signUpCredentials => {
+  "email": "$testString@yandex.ru",
+  "password": "password",
+};
+
+Map<String, String> get baseCredentials => {
+  "email": "email123@yandex.ru",
+  "password": "password",
+};
+
+BaseUseCase useCase = BaseUseCase();
+
+void onResponse(Object obj, Type expectedType) {
+  assert(obj.runtimeType == expectedType);
+}
+
+void onError(String e) {
+  fail(e);
+}
+
+void main() {
+  group("TestQuery", () {
+    test("SignUp", () async {
+      await useCase.signup(
+        email: signUpCredentials["email"]!,
+        password: signUpCredentials["password"]!,
+        onResponse: (obj) => onResponse(obj, AuthModel),
+        onError: onError,
+      );
+    });
+    test("Login", () async {
+      await useCase.login(
+        email: baseCredentials["email"]!,
+        password: baseCredentials["password"]!,
+        onResponse: (obj) => onResponse(obj, AuthModel),
+        onError: onError,
+      );
+    });
+    test("GetItemsList", () async {
+      await useCase.getItemsList(
+        onResponse: (obj) => onResponse(obj, List<ItemModel>),
+        onError: onError,
+      );
+    });
+  });
+}
