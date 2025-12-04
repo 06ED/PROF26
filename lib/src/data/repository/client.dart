181a977365943a91b4c0158719568db2b2eeba41
Denis Titovets <den232titovets@yandex.ru>
2025-12-04 21:49:10 +0300

    Реализация интерфейса в `Client`

---
+++ lib/src/data/repository/client.dart
@@ -0,0 +1,82 @@
+import 'package:dio/dio.dart';
+import 'package:prof_26_query/prof_26_query.dart';
+
+class Client implements Repository {
+  final Dio _dio;
+  final Storage _storage;
+
+  Client({required Dio dio, required Storage storage})
+    : _dio = dio,
+      _storage = storage;
+
+  @override
+  String get usersURL => "${_storage.apiURL}/collections/users/";
+
+  @override
+  String get itemsURL => "${_storage.apiURL}/collections/items/";
+
+  @override
+  Options get options => Options(
+    headers: {if (lastAuth != null) "Authorization": lastAuth!.token},
+  );
+
+  @override
+  AuthModel? lastAuth;
+
+  @override
+  Future<void> signup({
+    required String email,
+    required String password,
+    required String passwordConfirm,
+  }) async {
+    // TODO: implement signup
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthModel> login({
+    required String identity,
+    required String password,
+  }) async {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ItemModel> getItemByID({required String id}) async {
+    // TODO: implement getItemByID
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ItemModel>> getItemsList() async {
+    // TODO: implement getItemsList
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ItemModel>> searchItemsByTitle({required String search}) async {
+    // TODO: implement searchItemsByTitle
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ItemModel>> searchItemsByDescription({
+    required String search,
+  }) async {
+    // TODO: implement searchItemsByDescription
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ItemModel>> getOrderedByPriceItems() async {
+    // TODO: implement getOrderedByPriceItems
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<ItemModel>> getOrderedByCreatedItems() async {
+    // TODO: implement getOrderedByCreatedItems
+    throw UnimplementedError();
+  }
+}
