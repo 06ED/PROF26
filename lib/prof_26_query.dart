fea14bbe067cb8d6df1d67011f6673adfc6b0dc4
Denis Titovets <den232titovets@yandex.ru>
2025-12-06 16:21:34 +0300

    Добавление интерфейса `Repository`

--- lib/prof_26_query.dart
+++ lib/prof_26_query.dart
@@ -1,1 +1,2 @@
 export "src/data/models/auth_model.dart";
+export "src/data/repository/repository.dart";
---
+++ lib/src/data/repository/repository.dart
@@ -0,0 +1,14 @@
+import "package:dio/dio.dart";
+import "package:prof_26_query/prof_26_query.dart";
+
+abstract interface class Repository {
+  abstract final String authURL;
+  abstract final String apiKey;
+  abstract final Options options;
+
+  abstract AuthModel? lastAuth;
+
+  Future<AuthModel> signup({required String email, required String password});
+
+  Future<AuthModel> login({required String email, required String password});
+}
