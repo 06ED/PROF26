c88eab18cf39e7456a9815331c614ae2672297b1
Denis Titovets <den232titovets@yandex.ru>
2025-12-06 16:18:42 +0300

    Добавление `AuthModel`

---
+++ lib/src/data/models/auth_model.dart
@@ -0,0 +1,24 @@
+class AuthModel {
+  final String accessToken;
+  final String tokenType;
+  final int expiresIn;
+  final int expiresAt;
+  final String refreshToken;
+
+  AuthModel({
+    required this.accessToken,
+    required this.tokenType,
+    required this.expiresIn,
+    required this.expiresAt,
+    required this.refreshToken,
+  });
+
+  AuthModel.fromJSON(Map<String, dynamic> json)
+    : this(
+        accessToken: json["access_token"],
+        tokenType: json["token_type"],
+        expiresIn: json["expires_in"],
+        expiresAt: json["expires_in"],
+        refreshToken: json["refresh_token"],
+      );
+}
