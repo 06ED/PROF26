31555404a6d0a554f1869ba78f3bcc7fcee2fe00
Denis Titovets <den232titovets@yandex.ru>
2025-12-06 16:41:02 +0300

    Добавление тестирования

--- test/prof_26_query_test.dart
+++ test/prof_26_query_test.dart
@@ -1,12 +1,40 @@
-// import 'package:flutter_test/flutter_test.dart';
-//
-// import 'package:prof_26_query/prof_26_query.dart';
-//
-// void main() {
-//   test('adds one to input values', () {
-//     final calculator = Calculator();
-//     expect(calculator.addOne(2), 3);
-//     expect(calculator.addOne(-7), -6);
-//     expect(calculator.addOne(0), 1);
-//   });
-// }
+import "package:prof_26_query/prof_26_query.dart";
+import "package:flutter_test/flutter_test.dart";
+
+String testString = DateTime.now().microsecondsSinceEpoch.toString();
+
+Map<String, String> get credentials => {
+  "email": "$testString@yandex.ru",
+  "password": "password",
+};
+
+BaseUseCase useCase = BaseUseCase();
+
+void onResponse(dynamic obj, Type expectedType) {
+  assert(obj.runtimeType == expectedType);
+}
+
+void onError(String e) {
+  fail(e);
+}
+
+void main() {
+  group("TestQuery", () {
+    test("SignUp", () async {
+      await useCase.signup(
+        email: credentials["email"]!,
+        password: credentials["password"]!,
+        onResponse: (obj) => onResponse(obj, AuthModel),
+        onError: onError,
+      );
+    });
+    test("Login", () async {
+      await useCase.login(
+        email: credentials["email"]!,
+        password: credentials["password"]!,
+        onResponse: (obj) => onResponse(obj, AuthModel),
+        onError: onError,
+      );
+    });
+  });
+}
