6b5d93bb6240c2d7670b431115846c28814bff1c
Denis Titovets <den232titovets@yandex.ru>
2026-01-21 20:22:59 +0300

    Добавление `QueryHelper`

--- lib/ai_notes_query.dart
+++ lib/ai_notes_query.dart
@@ -5,3 +5,5 @@ export 'src/data/models/user_model.dart';
 
 export 'src/data/repository/client.dart';
 export 'src/data/repository/repository.dart';
+
+export 'src/domain/services/query_helper.dart';
