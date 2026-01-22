1991e8358d595d33e2cf083aec75864281f1a5c8
Denis Titovets <den232titovets@yandex.ru>
2026-01-22 15:03:25 +0300

    Добавление `MaterialAppWrapper`

---
+++ lib/src/presentation/widgets/utils.dart
@@ -0,0 +1,46 @@
+import 'package:ai_notes_uikit/ai_notes_uikit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class MaterialAppWrapper extends StatelessWidget {
+  final Widget? widget;
+
+  const MaterialAppWrapper({super.key, this.widget});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: .new(376, 832),
+      child: MaterialApp(
+        theme: .light().copyWith(
+          extensions: [CustomTheme(palette: LightPalette())],
+        ),
+        darkTheme: .dark().copyWith(
+          extensions: [CustomTheme(palette: DarkPalette())],
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Builder(
+          builder: (BuildContext context) {
+            var theme = CustomTheme.of(context);
+
+            return Scaffold(
+              backgroundColor: theme.palette.background,
+              resizeToAvoidBottomInset: false,
+              body: Padding(
+                padding: .symmetric(horizontal: 16.w, vertical: 32.h),
+                child: Center(
+                  child:
+                      widget ??
+                      CircularProgressIndicator(
+                        backgroundColor: Colors.transparent,
+                        color: theme.palette.accent,
+                      ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
